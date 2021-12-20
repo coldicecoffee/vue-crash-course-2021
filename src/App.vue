e2377a785b28b250402ad29aa05ac15a6c9895f8
Jude Hokyoon Woo <kw021992@gmail.com>
2021-12-20 22:35:33 +0900

    Update App to import Footer

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,7 @@
 
 <script>
 import AddTask from "./components/AddTask.vue";
+import Footer from "./components/Footer.vue";
 import Header from "./components/Header.vue";
 import Tasks from "./components/Tasks.vue";
 
